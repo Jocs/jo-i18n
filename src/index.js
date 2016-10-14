c70e233bcac0fc4873a0513138b823722058123d
Jocs <luoran1988@126.com>
2016-10-14 11:41:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -35,3 +35,39 @@ export const __ = (...params) => {
 		default: return console.error('输入参数个数有误，‘__’函数只接受一到两个参数。')
 	}
 }
+
+export const translateProvider = (angular, name) => {
+	const getTexts = ele => {
+		const result = [];
+		(function getNodes(element) {
+			const childNodes = element.childNodes;
+			if (childNodes.length !== 0) {
+				childNodes.forEach(c => {
+					if ((c.nodeType === 3 && /\S/.test(c.nodeValue)) || (c.tagName === 'INPUT' && !c.hasAttribute('notranslate'))) result.push(c);
+					if (c.nodeType === 1 && !c.hasAttribute('notranslate')) getNodes(c);
+				});
+			}
+		})(ele);
+		return result;
+	};
+
+	TranslateCtrl.$inject = ['$element'];
+	function TranslateCtrl($element) {
+		const handler = () => {
+			const textNodes = getTexts($element[0]);
+			textNodes.forEach(t => {
+				if (t.tagName && t.tagName === 'INPUT') {
+					t.value = __(t.value);
+				} else t.nodeValue = __(t.nodeValue);
+			});
+		};
+		this.$doCheck = handler;
+	}
+
+	return angular.module('components[name]', [])
+		.directive(name, () => ({
+			restrict: 'A',
+			controller: TranslateCtrl
+		}))
+		.name;
+}
